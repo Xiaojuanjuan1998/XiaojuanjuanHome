ec4697a8e4e502af0dc2260f647494061b44344a
Administrator <Administrator@192.168.1.150>
2017-12-20 19:27:59 +0800

    xiaojuanjuan提交了作业

---
+++ src/main/java/dt59homework/Demo1.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:dt59homework
+ * File Name:Demo1.java
+ * Package Name:dt59homework
+ * Date:2017年12月20日下午7:04:35
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59homework;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:04:35 <br/>
+ * @author   Administrator
+ * @version
+ * @see
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+

a0a1ce755cb3ea12c4d7f9c80b4435b7db1802e9
Administrator <Administrator@192.168.1.150>
2017-12-20 19:53:51 +0800

    提交操作

---
+++ src/main/java/dt59homework/Demo2.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:dt59homework
+ * File Name:Demo2.java
+ * Package Name:dt59homework
+ * Date:2017年12月20日下午7:53:05
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59homework;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:53:05 <br/>
+ * @author   Administrator
+ * @version
+ * @see
+ */
+public class Demo2 {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
